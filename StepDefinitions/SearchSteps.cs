00d32ca226cc1cbbdecfd98e54652ee0f8c2baf8
Rachaelfavour <60661756+Rachaelfavour@users.noreply.github.com>
2021-10-18 03:44:06 +0100

    Add files via upload
    Task

---
+++ StepDefinitions/SearchSteps.cs
@@ -0,0 +1,58 @@
+﻿using IMDBProject.PageObjects;
+using System;
+using TechTalk.SpecFlow;
+
+namespace IMDBProject.StepDefinitions
+{
+    [Binding]
+    public class SearchSteps
+    {
+       
+        SearchPageObject search;
+
+        public SearchSteps()
+        {
+            search = new SearchPageObject();
+        }
+
+        [Given(@"I navigate to '(.*)'")]
+        public void GivenINavigateTo(string url)
+        {
+            search.Navigate(url);
+        }
+        
+        [When(@"I search for genre '(.*)'")]
+        public void WhenISearchForGenre(string genre)
+        {
+           search.Genre(genre);
+        }
+        
+        [When(@"I sort for ""(.*)""")]
+        public void WhenISortFor(string criteria)
+        {
+          search.SortBy(criteria);
+        }
+        
+        [Then(@"I should see relevant result displayed")]
+        public void ThenIShouldSeeRelevantResultDisplayed()
+        {
+           search.Result();
+        }
+        
+        [Then(@"I should see the list of movies displyed in order of release date")]
+        public void ThenIShouldSeeTheListOfMoviesDisplyedInOrderOfReleaseDate()
+        {
+            search.ReleaseDate();
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
